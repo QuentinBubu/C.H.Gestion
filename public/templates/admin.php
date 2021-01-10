706bee4db2192b4f80e6437a8ac0c7c748166adf
QuentinBubu <anagamesprint@gmail.com>
2021-01-10 23:32:09 +0100

    Delete return line and add space

--- public/templates/admin.php
+++ public/templates/admin.php
@@ -21,9 +21,8 @@ if (!isset($user) || $user->getInformation('service') !== 'administration') {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Espace administrateur</title>
 </head>
 <body>
